dc56966b5ffb36fcd0ad688f61cb9a62b6a27acb
Wycliffe <wycliffekiprono4@gmail.com>
2023-05-17 13:59:52 +0300

    delete attribute

--- contacts.rb
+++ contacts.rb
@@ -18,16 +18,25 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
-  contacts.each do |person, data|
-
-    data.each do |attribute, value|
-
-      data[:favorite_ice_cream_flavors].delete("strawberry")
-      end 
-      contacts
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
     end
-end
+  end
+end 
+#   contacts.each do |person, data|
+
+#     # data.each do |attribute, value|
+
+#     #   data[:favorite_ice_cream_flavors].delete("strawberry")
+#     #   end 
+#     #   contacts
+#     # end
+# end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
