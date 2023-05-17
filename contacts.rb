7c7afdd45d481fb88970168aa20c07236411730d
Wycliffe <wycliffekiprono4@gmail.com>
2023-05-17 13:39:02 +0300

    done

--- contacts.rb
+++ contacts.rb
@@ -19,6 +19,14 @@ end
 
 def remove_strawberry(contacts)
   # your code here!
+  contacts.each do |person, data|
+
+    data.each do |attribute, value|
+
+      data[:favorite_ice_cream_flavors].delete("strawberry")
+      end 
+      contacts
+    end
 end
 
 # print the output to the terminal for inspection
